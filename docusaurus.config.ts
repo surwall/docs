d0dcc53180704a47e453c003ca636bb59caa15a0
Marcus Xu <surwall07@gmail.com>
2024-01-27 23:22:19 +0800

    deploy settings

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,15 +10,15 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://docs.xuchaoyin.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  // organizationName: 'surwall', // Usually your GitHub org/user name.
+  // projectName: 'docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -114,6 +114,7 @@ const config: Config = {
       apiKey: 'c10fda5ca69e614cec97a98a6ffda68d',
       indexName: 'xuchaoyin'
     },
+    
     footer: {
       style: 'dark',
       links: [
